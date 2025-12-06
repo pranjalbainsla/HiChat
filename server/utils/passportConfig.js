cf1f289805bafd2f0ab18f3529861a1e5c009589
Pranjal Bainsla <pranjalbainsla@kushagras-MacBook-Air-4.local>
2025-12-06 18:28:12 +0530

    google callback fixed

--- server/utils/passportConfig.js
+++ server/utils/passportConfig.js
@@ -5,7 +5,7 @@ const User = require('../models/user');
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "/api/auth/google/callback"
+    callbackURL: "https://hichat-tunu.onrender.com/api/auth/google/callback"
   },
   async (accessToken, refreshToken, profile, done) => {
     //console.log('ACCESS TOKEN:', accessToken);
